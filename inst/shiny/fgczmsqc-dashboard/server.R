eeeaf73c1a296924f39728b598b3b733e7f68117
Christian Panse <cp@fgcz.ethz.ch>
2023-10-27 16:14:40 +0200

    add irt support

--- inst/shiny/fgczmsqc-dashboard/server.R
+++ inst/shiny/fgczmsqc-dashboard/server.R
@@ -20,18 +20,56 @@ stopifnot(require(readr), require(reshape2), require(shinydashboard))
     plot()
 }
 
-.iRTprofile <- function(f){
-  plot(0, 0, sub=f, type = 'n', xlab='', ylab=''); text(0,0, "t.b.d", cex=5)
+.missing <- function(){
+  plot(0,0, xlab = '', ylab = '', type = 'n', axes = FALSE)
+  text(0, 0, "missing\ndata or plot", cex=5)
 }
+
 # define server logic ============
 function(input, output, session) {
-  
   #  reactives =============
+  iRTprofileRawDDA <- reactive({
+    iRTmz <- c(487.2571, 547.2984, 622.8539, 636.8695, 644.8230, 669.8384, 683.8282,
+               683.8541, 699.3388, 726.8361, 776.9301)
+    
+    names(iRTmz) <- c("LGGNEQVTR", "YILAGVENSK", "GTFIIDPGGVIR", "GTFIIDPAAVIR",
+                      "GAGSSEPVTGLDAK", "TPVISGGPYEYR", "VEATFGVDESNAK",
+                      "TPVITGAPYEYR", "DGLDAASYYAPVR", "ADVTPADFSEWSK",
+                      "LFLQFGAQGSPFLK")
+    
+    rawrr::readChromatogram(input$file, mass = iRTmz, tol = as.integer(input$ppmError), type = "xic", filter = "ms") 
+  })
+  
+  iRTprofileRawDIA <- reactive({
+    #plot(0, 0, sub=f, type = 'n', xlab='', ylab=''); text(0,0, "t.b.d", cex=5)
+    iRTmz <- c(487.2571, 547.2984, 622.8539, 636.8695, 644.8230, 669.8384, 683.8282,
+               683.8541, 699.3388, 726.8361, 776.9301)
+    
+    names(iRTmz) <- c("LGGNEQVTR", "YILAGVENSK", "GTFIIDPGGVIR", "GTFIIDPAAVIR",
+                      "GAGSSEPVTGLDAK", "TPVISGGPYEYR", "VEATFGVDESNAK",
+                      "TPVITGAPYEYR", "DGLDAASYYAPVR", "ADVTPADFSEWSK",
+                      "LFLQFGAQGSPFLK")
+    
+    yIonSeries <- names(iRTmz)[9] |>
+      lapply(function(x){
+        y <- protViz::fragmentIon(x)[[1]]$y
+        y[seq(1, length(y) - 1)]
+      }) |> unlist()
+    
+    input$file |>
+      rawrr::readChromatogram(mass = yIonSeries, tol = 15, type = "xic", filter = "ms2") 
+  })
+  
+ 
   wide <- reactive({
-    S <- readr::read_delim("~/Downloads/output.txt", 
-                      delim = ";", escape_double = FALSE, col_types = cols(Time = col_datetime(format = "%s"), 
-                                                                           Size = col_integer(), Precursors.Identified = col_integer(), 
-                                                                           Proteins.Identified = col_integer()), 
+    S <- rootdir() |> 
+      file.path("output.txt") |> readr::read_delim(
+                      delim = ";",
+                      escape_double = FALSE,
+                      col_types = cols(Time = col_datetime(format = "%s"),
+                                       Size = col_integer(),
+                                       Precursors.Identified = col_integer(),
+                                       Proteins.Identified = col_integer()), 
                       trim_ws = TRUE) 
     
    
@@ -53,24 +91,31 @@ function(input, output, session) {
   })
   
   data <- reactive({
-    
     now <- Sys.time()
-    
     long()[long()$Instrument %in% input$instrument &
              long()$variable %in% input$variables &
              input$days[1] <=  difftime(now, long()$Time, units = "days") &
              difftime(now, long()$Time, units = "days") < input$days[2], ]
   }) 
   
-  rootdir <- reactive({"/Users/cp/Downloads/dump/"})
+  rootdir <- reactive({
+    cands <- c("/Users/cp/Downloads/dump/", "/scratch/DIAQC/qc/dump")
+    for (d in cands){
+      if (dir.exists(d))return(d)
+    }
+    NULL
+    })
   
   files <- reactive({
-    
     data()$File.Name |>
       unique() |>
+      lapply(function(f){file.path(rootdir(), f)}) |>
       lapply(function(f){
-        if(file.exists(file.path(rootdir(), f)))
-          return(f)
+        if(file.exists(f)){return(f)}
+        else{
+          msg <- paste0("Can not find file ", f)
+          message(msg)
+        }
         NULL
         }) |>
       unlist()
@@ -101,19 +146,62 @@ function(input, output, session) {
   
   # renderPlots ==========
   output$plot1 <- renderPlot({
-    if(input$method == "DIANN"){
+    if(data() |> nrow() > 0){
       lattice::xyplot(value ~ Time | variable,
                       group = Instrument,
                       data = data(),
                       scales = 'free',
                       type = 'b',
                       layout = c(1, length(input$variables)),
-                      auto.key = list(space = "bottom"))
-    }else if (input$method == "TIC"){
-      .tic(file.path(rootdir(),input$file))
-    }
-    else{
-      .iRTprofile(input$file)
-    }
+                      auto.key = list(space = "bottom"))}
+    else{.missing()}
+    
   })
+  
+  output$table <- renderDataTable({ data() })
+  
+  output$plotiRTDDAChromatograms <- renderPlot({
+    iRTprofileRawDDA() |> plot(main=input$file)
+    
+  })
+  
+  output$plotiRTprofiles <- renderPlot({
+    par(mfrow = c(2, 6), mar=c(4,4,4,1))
+    rtFittedAPEX <- iRTprofileRawDDA() |>
+      rawrr:::pickPeak.rawrrChromatogram() |>
+      rawrr:::fitPeak.rawrrChromatogram() |>
+      lapply(function(x){
+        plot(x$times, x$intensities, type='p',
+             ylim=range(c(x$intensities,x$yp)),
+             main=x$mass); lines(x$xx, x$yp,
+                                 col='red'); x}) |>
+      sapply(function(x){x$xx[which.max(x$yp)[1]]})
+  })
+  
+  output$plotiRTfits <- renderPlot({
+    iRTscore <- c(-24.92, 19.79, 70.52, 87.23, 0, 28.71, 12.39, 33.38, 42.26, 54.62, 100)
+    
+    rtFittedAPEX <- iRTprofileRawDDA() |>
+      rawrr:::pickPeak.rawrrChromatogram() |>
+      rawrr:::fitPeak.rawrrChromatogram() |>
+      sapply(function(x){x$xx[which.max(x$yp)[1]]})
+    
+    fit <- lm(rtFittedAPEX ~ iRTscore)
+    par(mfrow = c(1, 1), mar=c(5,5,4,1))
+    plot(rtFittedAPEX ~ iRTscore,
+         ylab = 'Retention time [min]',
+         xlab = "iRT score",
+         pch=16, frame.plot = FALSE)
+    abline(fit, col = 'grey')
+    abline(v = 0, col = "grey", lty = 2)
+    legend("topleft", legend = paste("Regression line: ", "rt =",
+                                     format(coef(fit)[1], digits = 4), " + ",
+                                     format(coef(fit)[2], digits = 2), "score",
+                                     "\nR2: ", format(summary(fit)$r.squared, digits = 4)),
+           bty = "n", cex = 0.75)
+    #text(iRTscore, rt, iRTmz, pos=1,cex=0.5)
+  })
+  
+  output$plotiRTfits2 <- renderPlot({plot(0,0)})
+
 }
--- inst/shiny/fgczmsqc-dashboard/ui.R
+++ inst/shiny/fgczmsqc-dashboard/ui.R
@@ -5,30 +5,59 @@ library(shinydashboard)
 dashboardPage(
   dashboardHeader(title = "FCGZ MS QC"),
   dashboardSidebar(
-    htmlOutput("instrument"),
-    htmlOutput("variable"),
-    htmlOutput("file"),
-    radioButtons(
-      "method",
-      "Method",
-      choices = c("DIANN", "iRT profile", "TIC"),
-      selected = c("DIANN"),
-      inline = FALSE,
-      width = NULL
-  #    choiceNames = c("DIANN"),
-  #    choiceValues = c("DIANN")
-    ),
-  br(),
-  a(img(src="https://img.shields.io/badge/JIB-10.1515%2Fjib.2022.0031-brightgreen"),
-    href='https://www.degruyter.com/document/doi/10.1515/jib-2022-0031/html'),
-  a(img(src="https://img.shields.io/badge/JPR-10.1021%2Facs.jproteome.0c00866-brightgreen"),
-    href='http://dx.doi.org/10.1021/acs.jproteome.0c00866')
+    sidebarMenu(
+      menuItem("DIA-NN stat", tabName = "diannplots", icon = icon("chart-line")),
+      menuItem("DIA-NN stat data", tabName = "dianndata", icon = icon("table")),
+      menuItem("iRT profiles", tabName = "iRTprofiles", icon = icon("chart-line")),
+      menuItem("TIC", tabName = "tic", icon = icon("chart-line")),
+      htmlOutput("instrument"),
+      htmlOutput("variable"),
+      htmlOutput("file")),
+    br(),
+    a(img(src="https://img.shields.io/badge/JIB-10.1515%2Fjib.2022.0031-brightgreen"),
+      href='https://www.degruyter.com/document/doi/10.1515/jib-2022-0031/html'),
+    a(img(src="https://img.shields.io/badge/JPR-10.1021%2Facs.jproteome.0c00866-brightgreen"),
+      href='http://dx.doi.org/10.1021/acs.jproteome.0c00866')
   ),
   dashboardBody(
     # Boxes need to be put in a row (or column)
-    fluidRow(
-      sliderInput("days", "Observation range in days:", min = 0, max = 365, value = c(0, 28), width = 1000),
-      box(plotOutput("plot1", height = 500, width = 1000))
+    tabItems(
+      # First tab content
+      tabItem(tabName = "diannplots",
+              fluidRow(
+                sliderInput("days", "Observation range in days:", min = 0, max = 365, value = c(0, 28), width = 1000),
+                box(plotOutput("plot1", height = 500, width = 1000))
+              )
+      ),
+      # Second tab content
+      tabItem(tabName = "dianndata",
+              fluidRow(
+                h2("DIA-NN stat.tsv"),
+                dataTableOutput('table')
+              )
+      ),
+      tabItem(tabName = "iRTprofiles",
+              fluidRow(
+                box(plotOutput("plotiRTDDAChromatograms", height = 300, width = 1000),
+                )
+                
+              ),
+              fluidRow(
+                
+                box(plotOutput("plotiRTfits", height = 300, width = 300))
+              ),
+              fluidRow(
+               
+                  box(plotOutput("plotiRTprofiles", height = 400, width = 1000))
+                
+              ),
+              fluidRow(radioButtons("ppmError", "ppmError",
+                                    choices = c(10, 15, 20, 100),
+                                    selected = 10,
+                                    inline = TRUE,
+                                    width = NULL
+              ))
+      )
     )
   )
 )
