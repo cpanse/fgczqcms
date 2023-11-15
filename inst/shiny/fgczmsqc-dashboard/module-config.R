9d9441afa072190a3f385d93feb3b7d5276c0db8
Christian Panse <cp@fgcz.ethz.ch>
2023-11-15 12:29:57 +0100

    add config module #13

---
+++ inst/shiny/fgczmsqc-dashboard/module-config.R
@@ -0,0 +1,25 @@
+#R
+
+configServer <- function(id){
+  moduleServer(id,
+               function(input, output, session) {
+                 rootdirraw <- reactive({
+                   cands <- c("/srv/www/htdocs/", "/scratch/DIAQC/qc/dump", "/Users/cp/Downloads/dump/")
+                   for (d in cands){
+                     if (dir.exists(d))return(d)
+                   }
+                   NULL
+                 })
+                 
+                 rootdir <- reactive({
+                   cands <- c("/Users/cp/Downloads/dump/", "/scratch/DIAQC/qc/dump")
+                   for (d in cands){
+                     if (dir.exists(d))return(d)
+                   }
+                   NULL
+                 })
+                 return(list(rootdir = rootdir(),
+                             rootdirraw = rootdirraw()))
+               }
+  )
+}
