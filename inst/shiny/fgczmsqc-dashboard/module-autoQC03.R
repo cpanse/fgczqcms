2cd0844374d8fa0d3099b2f9a1bea28ea419dd1b
Christian Panse <cp@fgcz.ethz.ch>
2023-11-20 10:47:21 +0100

    use ggplot2 for autoQC03

--- inst/shiny/fgczmsqc-dashboard/module-autoQC03.R
+++ inst/shiny/fgczmsqc-dashboard/module-autoQC03.R
@@ -37,8 +37,8 @@ autoQC03Server <- function(id, filterValues, BFabric, inputfile, readFUN, title)
                  output$variable <- renderUI({
                    
                    defaulVariablesIdx <-  dataVariables() %in% c('nConfidentProteins',
-                            'nConfidentPeptides', 'nMS2', 'Precursors.Identified',
-                            'Proteins.Identified', 'FWHM.RT')  |> which()
+                                                                 'nConfidentPeptides', 'nMS2', 'Precursors.Identified',
+                                                                 'Proteins.Identified', 'FWHM.RT')  |> which()
                    
                    shinydashboard::box(
                      selectInput(ns('variables'), 'Variables',
@@ -57,52 +57,57 @@ autoQC03Server <- function(id, filterValues, BFabric, inputfile, readFUN, title)
                    message(paste0("nrow data(): ", nrow(data())))
                    filter <- data()$Instrument %in% filterValues$instrument &
                      filterValues$timeMin < data()$time & data()$time < filterValues$timeMax 
-                     #data()$variable %in% input$Variables 
+                   #data()$variable %in% input$Variables 
                    
                    data()[filter,]
                  })
                  
-                   output$ui <- renderUI({
-                     tagList(
-                       h2(title),
-                       fluidRow(htmlOutput(ns("variable"))),
-                       shinydashboard::box(
-                         title = paste0(title, " plots"),
-                         status = "primary",
-                         solidHeader = TRUE,
-                         collapsible = TRUE,
-                         width = 12,
-                         plotOutput(ns("plot"))
-                       ),
-                     )
-                   })
+                 output$ui <- renderUI({
+                   tagList(
+                     h2(title),
+                     fluidRow(htmlOutput(ns("variable"))),
+                     shinydashboard::box(
+                       title = paste0(title, " plots"),
+                       status = "primary",
+                       solidHeader = TRUE,
+                       collapsible = TRUE,
+                       width = 12,
+                       plotOutput(ns("plot"))
+                     ),
+                   )
+                 })
+                 
+                 output$plot <- renderPlot({
+                   shiny::req(dataFiltered())
                    
-                   output$plot <- renderPlot({
-                     shiny::req(dataFiltered())
+                   message(paste0("nrow dataFiltered(): ", nrow(dataFiltered())))
+                   dataFiltered() |> head() |> print()
+                   
+                   #lattice::xyplot(value ~ time | Instrument * variable,
+                   #                 subset = dataFiltered()$variable %in% input$variables,
+                   #                data = dataFiltered())
+                   dataFiltered() |> 
+                     subset(variable %in% input$variables) |> 
+                     ggplot2::ggplot(ggplot2::aes(time, value)) +
+                     ggplot2::facet_wrap(. ~  Instrument * variable, scales="free_y", ncol = 1)  -> gp
+                   
+                   if ("scanType" %in% names(dataFiltered())){
+                     gp +
+                       ggplot2::geom_point(ggplot2::aes(time, value, colour = factor(scanType)), alpha = 0.4) +
+                       ggplot2::geom_line(ggplot2::aes(time, value, colour = factor(scanType)), alpha = 0.4) -> gp
                      
-                     message(paste0("nrow dataFiltered(): ", nrow(dataFiltered())))
-                     dataFiltered() |> head() |> print()
-                     
-                     #lattice::xyplot(value ~ time | Instrument * variable,
-                     #                 subset = dataFiltered()$variable %in% input$variables,
-                     #                data = dataFiltered())
-                     subset(dataFiltered(), variable %in% input$variables) |> 
-                       ggplot2::ggplot(ggplot2::aes(time, value)) +
+                   }else{
+                     gp +
                        ggplot2::geom_point(ggplot2::aes(time, value), alpha = 0.4) +
-                       #ggplot2::geom_line(ggplot2::aes(group = peptide, color = peptide), alpha = 0.4) +
-                       #ggplot2::scale_y_log10() +
-                       ggplot2::facet_wrap(. ~  Instrument * variable, scales="free_y", ncol = 1)  -> gp
-                     
-                     if ("scanType" %in% names(dataFiltered())){
-                       gp + ggplot2::geom_line(ggplot2::aes(group = "scanType", color = "scanType"), alpha = 0.4) -> gp
-                     }
-                     
-                     if (filterValues$useBFabric){
-                       gp + ggplot2::geom_vline(xintercept = BFabric$bfabricInstrumentEventsFiltered()$time, linetype="dashed", 
-                                                color = "red", size = 1) -> gp
-                     }
-                     gp
-                   })
+                       ggplot2::geom_line(ggplot2::aes(time, value), alpha = 0.4) -> gp
+                   }
                    
+                   if (filterValues$useBFabric){
+                     gp + ggplot2::geom_vline(xintercept = BFabric$bfabricInstrumentEventsFiltered()$time, linetype="dashed", 
+                                              color = "red", size = 1) -> gp
+                   }
+                   gp
+                 })
+                 
                })
 }
